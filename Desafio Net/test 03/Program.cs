6001a126fd715ad52c3fdf102a86473eff17ed4d
niocp <57359997+niocp@users.noreply.github.com>
2023-07-13 21:41:02 -0300

    Add files via upload

---
+++ Desafio Net/test 03/Program.cs
@@ -0,0 +1,37 @@
+﻿int loop = 0;
+double nota1, nota2, media;
+string nome, resultado;
+
+Console.WriteLine("Seja bem vindo ao 3º teste! " + Environment.NewLine);
+
+while (loop == 0) {
+
+    Console.WriteLine("Por gentileza, informe o nome do aluno em questão: ");
+
+    nome = Console.ReadLine();
+
+    Console.WriteLine("Digite a nota do 1º semestre: ");
+
+    nota1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite a nota do 2º semestre: ");
+
+    nota2 = int.Parse(Console.ReadLine());
+
+    media = (nota1 + nota2) / 2;
+
+    if (media < 6.5) {
+
+        resultado = "Reprovado";
+
+    } else {
+
+        resultado = "Aprovado";
+
+    }
+
+    Console.WriteLine($"Nome: {nome}");
+    Console.WriteLine($"A média é: {media}");
+    Console.WriteLine($"O aluno foi: {resultado}" + Environment.NewLine);
+
+}
---
+++ Desafio Net/test 2/Program.cs
@@ -0,0 +1,26 @@
+﻿int qtdProva = 0;
+double nota, media, totalNota = 0;
+string nome;
+
+Console.WriteLine("Boas vindas ao teste 02! Para inicio de conversa, digite seu nome por gentileza: ");
+
+    nome = Console.ReadLine();
+
+while (nome != null) {
+
+    Console.WriteLine($"{nome} nos informe a nota que você tirou em sua prova: ");
+
+        nota = int.Parse(Console.ReadLine());
+
+        qtdProva++;
+
+        totalNota = totalNota + nota;
+
+        media = totalNota / qtdProva;
+
+    Console.WriteLine($"Nome: {nome}");
+    Console.WriteLine($"Quantidade de Provas: {qtdProva}");
+    Console.WriteLine($"Total de Nota: {totalNota}");
+    Console.WriteLine($"Sua média é de: {media}" + Environment.NewLine);
+
+}
