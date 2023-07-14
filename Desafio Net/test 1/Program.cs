489b0814f06d06f22c7e2263f7acec6db9ce8221
niocp <57359997+niocp@users.noreply.github.com>
2023-07-13 21:38:49 -0300

    Add files via upload

---
+++ Desafio Net/test 1/Program.cs
@@ -0,0 +1,13 @@
+﻿double nota1, nota2, media;
+
+Console.WriteLine("Digite a nota do primeiro semestre: ");
+
+nota1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a nota do segundo semestre: ");
+
+nota2 = int.Parse(Console.ReadLine());
+
+media = (nota1 + nota2) / 2;
+
+Console.WriteLine($"A média calculada foi de {media}");
